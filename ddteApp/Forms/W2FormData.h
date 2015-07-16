56cce3475097247f530c63f53df1e63def90e87f
Fushiki <ifushiki@MTVL130e97867.local>
2015-07-16 15:41:15 -0700

    Created Froms directory and Curl files are moved to REST folder.

---
+++ ddteApp/Forms/W2FormData.h
@@ -0,0 +1,13 @@
+//
+//  W2FormData.h
+//  ddteApp
+//
+//  Created by Fushiki, Ikko on 7/16/15.
+//  Copyright (c) 2015 Fushiki, Ikko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface W2FormData : NSObject
+
+@end
---
+++ ddteApp/Forms/W2FormData.m
@@ -0,0 +1,13 @@
+//
+//  W2FormData.m
+//  ddteApp
+//
+//  Created by Fushiki, Ikko on 7/16/15.
+//  Copyright (c) 2015 Fushiki, Ikko. All rights reserved.
+//
+
+#import "W2FormData.h"
+
+@implementation W2FormData
+
+@end
